b8f06af841362878c04c5109259c55c93cb84789
Ekaterina Kuznetsova <kuznetsova.cat_37@mail.ru>
2024-10-24 20:47:46 +0300

    Forth commit, with corrections

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,34 +1,24 @@
 package praktikum;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 import static praktikum.Constants.DELTA;
 
-@RunWith(MockitoJUnitRunner.class)
-public class BunTest {
-    @Mock
-    Database database;
 
-    @Test //проверка метода на получение имени
+public class BunTest {
+
+    @Test //проверка получения верного названия булочки
     public void getName() {
-        Bun expectedBun = new Bun("black bun", 300F);
-        when(database.availableBuns()).thenReturn(List.of(expectedBun));
-        Bun bun = new Bun(database.availableBuns().get(0).name, database.availableBuns().get(0).price);
+        Database data = new Database();
+        Bun bun = new Bun(data.availableBuns().get(0).name, data.availableBuns().get(0).price);
         assertEquals("Bun name should be: \"black bun\"", "black bun", bun.getName());
     }
 
-    @Test //проверка метода на получение цены
+    @Test  //проверка получения верной цены булочки
     public void getPrice() {
-        Bun expectedBun = new Bun("black bun", 300F);
-        when(database.availableBuns()).thenReturn(List.of(expectedBun));
-        Bun bun = new Bun(database.availableBuns().get(0).name, database.availableBuns().get(0).price);
-        assertEquals("Bun price should be: \"300\"", 300F, bun.getPrice(), DELTA);
+        Database data = new Database();
+        Bun bun = new Bun(data.availableBuns().get(2).name, data.availableBuns().get(2).price);
+        assertEquals("Bun price should be: \"300\"", 300, bun.getPrice(), DELTA);
     }
 }
